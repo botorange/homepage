9f509dd595edb83ba90ab05716e36f1656c79d07
mengdi <liumengdidd@gmail.com>
2018-11-19 17:52:03 +0800

    bug fix

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(function() {
 
     function main() {
         initSwiper()
+        initScroll()
     }
 
     function initSwiper() {
@@ -13,6 +14,22 @@ $(function() {
             paginationClickable: true,
             autoHeight: true
         });
+    }
+
+    function initScroll() {
+        if(window.innerWidth <= 800) {
+            var wrapper3 = document.getElementById('logo-wrapper3');
+            wrapper3.scrollTo(230, 0);
+
+            var wrapper2 = document.getElementById('logo-wrapper2');
+            wrapper2.scrollTo(30, 0);
+
+            var wrapper1 = document.getElementById('logo-wrapper1');
+            wrapper1.scrollTo(30, 0);
+
+
+
+        }
     }
 
 });
