59e694ee79e4b53e8f3bebf5c5f0a9c297b4b1be
gengchenhui <gengchenhui@gmail.com>
2019-09-28 10:26:56 +0800

    upgrade to new version

--- js/index.js
+++ js/index.js
@@ -4,16 +4,46 @@ $(function() {
     function main() {
         initSwiper()
         initScroll()
+        // initAbility()
     }
 
     function initSwiper() {
-        var banner = new Swiper('.swiper-container', {
-            autoplay: 40000,
-            loop: true,
-            pagination: '.swiper-container .swiper-pagination',
-            paginationClickable: true,
-            autoHeight: true
-        });
+      var functionSwiper = new Swiper('.function-content', {
+          // autoplay: 40000,
+          // loop: true,
+          pagination: '.function-content .swiper-pagination',
+          paginationClickable: true,
+          autoHeight: true
+      });
+
+      var functionMobileSwiper = new Swiper('.function-mobile-content', {
+        // autoplay: 40000,
+        // loop: true,
+        pagination: '.function-mobile-content .swiper-pagination',
+        paginationClickable: true,
+        autoHeight: true
+    });
+      var ability = new Swiper('.ability-content-container', {
+        pagination:'.ability-title-container',
+        paginationClickable: true,
+        paginationBulletRender: function (index, className) {
+          var text = '';
+          switch(index){
+            case 0:text='销售赋能';break;
+            case 1:text='社群赋能';break;
+            case 2:text='客服赋能';break;
+          }
+          return '<div class="'+className+'"><div>'+ text + '</div><div class="short-line"></div></div>';
+        },
+      });
+      var allfun = new Swiper('.allfun-mobile-content', {
+        // autoplay: 40000,
+        // loop: true,
+        direction : 'horizontal',
+        pagination: '.allfun-mobile-pagination',
+        paginationClickable: true,
+        autoHeight: true
+      });
     }
 
     function initScroll() {
@@ -32,4 +62,13 @@ $(function() {
         }
     }
 
+    function initAbility() {
+      $('.ability-title').click(function(){
+        $('.ability-title-active').removeClass('ability-title-active');
+        $(this).addClass('ability-title-active');
+        var key = $(this).data('key');
+        $('.ability-content-active').removeClass('ability-content-active');
+        $('.ability-content-container [data-key='+key+']').addClass('ability-content-active');
+      });
+    }
 });
